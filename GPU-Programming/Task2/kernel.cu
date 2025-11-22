cd6d9426f00891a76557e107b5ba1e451d17c0c2
MNummila <maxnummila@gmail.com>
2025-11-22 14:08:58 +0200

    Add vector addition kernel for GPU

---
+++ GPU-Programming/Task2/kernel.cu
@@ -0,0 +1,13 @@
+__global__ void vecAddKernel(float *A, float *B, float *C, int n) {
+
+  // Calculate global thread index based on the block and thread indices ----
+
+    int i = blockIdx.x * blockDim.x + threadIdx.x;
+
+  // Use global index to determine which elements to read, add, and write ---
+
+  if (i < n) {
+    C[i] = A[i] + B[i];
+  }
+
+}
